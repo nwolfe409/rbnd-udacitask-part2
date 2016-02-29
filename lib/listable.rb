b3f62e314c275c9149704692d8c8dcc0eb5f2114
Nathan Wolfe <nwolfe409@gmail.com>
2016-02-29 04:11:34 +0000

    Project prep completed and working

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,22 @@
 module Listable
-  # Listable methods go here
+  def format_description(description)
+    "#{description}".ljust(30)
+  end
+  
+  def format_date(due)
+    @due ? @due.strftime("%D") : "No due date"
+    dates = @start_date.strftime("%D") if @start_date
+    dates << " -- " + @end_date.strftime("%D") if @end_date
+    dates = "N/A" if !dates
+    return dates
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
+
 end
